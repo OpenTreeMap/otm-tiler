e764358ec62a7260957d7e9da84c37dc8df58d96
Adam Hinz <hinz.adam@gmail.com>
2013-07-05 15:10:20 -0400

    Add framework for pseudo tests

---
+++ Makefile
@@ -0,0 +1,10 @@
+all:
+	npm install
+
+clean:
+	rm -rf node_modules/*
+
+check:
+	npm test
+
+test: check
---
+++ test/fake.js
@@ -0,0 +1,7 @@
+var assert = require("assert");
+
+describe('Do not fail', function() {
+    it('should not fail', function() {
+        assert.equal(1,1);
+    });
+});
