f007b20b490cb1e60e7ecc1a2f5bb218dcc0f5e1
Michael Maurizi <mmaurizi@azavea.com>
2013-07-18 17:25:53 -0400

    Add tests for filterStringToSelect.js

---
+++ test/testFilterStringToSelect.js
@@ -0,0 +1,21 @@
+var assert = require("assert");
+var filterStringToSelect = require("../filterStringToSelect");
+var config = require("../config.json");
+
+describe('filterStringToSelect', function() {
+
+    it('returns returns tree select when tree is in the filter string', function() {
+        var sql = filterStringToSelect('{"tree.id":{"IS":"1"}}');
+        assert.equal(sql, config.selectAllFieldsSql.tree);
+    });
+
+    it('returns returns tree select when species is in the filter string', function() {
+        var sql = filterStringToSelect('{"species.id":{"IS":"1"}}');
+        assert.equal(sql, config.selectAllFieldsSql.tree);
+    });
+
+    it('returns returns plot select when plot is in the filter string', function() {
+        var sql = filterStringToSelect('{"plot.id":{"IS":"1"}}');
+        assert.equal(sql, config.selectAllFieldsSql.plot);
+    });
+});
